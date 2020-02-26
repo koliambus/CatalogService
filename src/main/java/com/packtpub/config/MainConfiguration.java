f3f2a918eb485a13013436fd2bcc072ed8337700
koliambus <koliambus@ukr.net>
2020-02-26 23:27:01 +0200

    Adding Swagger 2

--- src/main/java/com/packtpub/config/MainConfiguration.java
+++ src/main/java/com/packtpub/config/MainConfiguration.java
@@ -20,10 +20,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @Import(EnvironmentConfiguration.class)
 @EnableWebMvc
+@EnableSwagger2
 public class MainConfiguration {
 
     private static final String STAGE_PROPERTY_NAME = "stage";
@@ -79,6 +85,15 @@ public class MainConfiguration {
                         new HttpHost(host, port, scheme)
                 )
         );
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/songs/**"))
+                .build();
     }
 
     private AmazonDynamoDB getDynamoDB() {
