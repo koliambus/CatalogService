8d50195b2013026ce6cbb2ef6686cb627d03325f
koliambus <koliambus@ukr.net>
2020-03-01 13:40:34 +0200

    fixed search functioning

--- src/main/java/com/packtpub/songs/search/SongsSearchService.java
+++ src/main/java/com/packtpub/songs/search/SongsSearchService.java
@@ -1,10 +1,14 @@
 package com.packtpub.songs.search;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.packtpub.songs.model.Song;
+import org.apache.http.util.EntityUtils;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Request;
 import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
@@ -26,24 +30,37 @@ public class SongsSearchService {
 
     public List<Song> searchSongs(String q) {
         List<Song> songs = new ArrayList<>();
-        SearchRequest searchRequest = new SearchRequest("published-songs");
-        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
 
-        if (q.isEmpty()) {
-            searchSourceBuilder.query(QueryBuilders.matchAllQuery());
-        } else {
-            // TODO fix query
-            searchSourceBuilder.query(QueryBuilders.wildcardQuery("id", q + "*"));
-        }
-
-        searchRequest.source(searchSourceBuilder);
-        try {
-            SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
-            for (SearchHit hit : searchResponse.getHits()) {
-                songs.add(new ObjectMapper().readValue(hit.getSourceAsString(), Song.class));
+        if (!q.isEmpty()) {
+            Request request = new Request("GET", "/published-songs/_search");
+            request.addParameter("q", q);
+            try {
+                Response response = restHighLevelClient.getLowLevelClient().performRequest(request);
+                String string = EntityUtils.toString(response.getEntity());
+                JsonNode root = new ObjectMapper().readTree(string);
+                for (JsonNode songWraper : root.at("/hits/hits")) {
+                    Song song = new ObjectMapper().readValue(songWraper.at("/_source").toString(), Song.class);
+                    songs.add(song);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } else {
+
+            SearchRequest searchRequest = new SearchRequest("published-songs");
+            SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+            searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+
+            searchRequest.source(searchSourceBuilder);
+            try {
+                SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+                for (SearchHit hit : searchResponse.getHits()) {
+                    songs.add(new ObjectMapper().readValue(hit.getSourceAsString(), Song.class));
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         return songs;
